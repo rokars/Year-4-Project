dfc81dc7b413690e261378f937d2287cb44142a1
Rokas <G00354068@gmit.ie>
2020-12-30 14:41:27 +0000

    Multiple Graphs
    Added few more graphs to shape the page

--- Project Web/readFileCharts/script.js
+++ Project Web/readFileCharts/script.js
@@ -23,12 +23,12 @@ function readerCallback(data) {
   jsonData = JSON.parse(data);
   var string = "";
 
-  for (var i = 0; i < jsonData.mock_data.length; i++) {
+  /*for (var i = 0; i < jsonData.mock_data.length; i++) {
     string += "<b> Time Stamp (UTC): </b>" + jsonData.mock_data[i].TimeStampDate + "<b> Altitude (m): </b>" + jsonData.mock_data[i].AltitudeQnh_meters +
               "<b> AirSpeed (knots): </b>" + jsonData.mock_data[i].AirSpeed_knots + "<b> Vertical Speed (m/s): </b>" + jsonData.mock_data[i].VerticalSpeed_ms +
               "<b> Normal Acceleration (g): </b>" + jsonData.mock_data[i].NormalAcceleration_g + "<b> Motor Power (v): </b>" + jsonData.mock_data[i].MotorPower_volts + "<br>";
   }
-  document.getElementById('data').innerHTML = string;
+  document.getElementById('data').innerHTML = string; */
   createGraph(jsonData);
 }
 
@@ -41,20 +41,20 @@ function createGraph(jsonData) {
     timeSorted.sort();
 
 
-  var data5 = [];
+  var altData = [];
   for(var j = 0; j < jsonData.mock_data.length; j++) {
-    data5.push({t: timeSorted[j], y: jsonData.mock_data[j].AltitudeQnh_meters});
+    altData.push({t: timeSorted[j], y: jsonData.mock_data[j].AltitudeQnh_meters});
   }
 
-let myChart5 = document.getElementById("myChart5").getContext('2d');
+let altitudeChart = document.getElementById("altitudeChart").getContext('2d');
 
-let chart5 = new Chart(myChart5, {
+let altChart = new Chart(altitudeChart, {
   type: 'line',
   data: {
     labels: timeSorted,
     datasets: [{
       label: 'Altitude Qnh (meters)',
-      data: data5,
+      data: altData,
       fill: false,
       borderColor: 'red',
       borderWidth: '0.5'
@@ -82,4 +82,89 @@ let chart5 = new Chart(myChart5, {
     }
   }
 });
+
+var aSpdData = [];
+for(var j = 0; j < jsonData.mock_data.length; j++) {
+  aSpdData.push({t: timeSorted[j], y: jsonData.mock_data[j].AirSpeed_knots});
+}
+
+let airSpeedChart = document.getElementById("airSpeedChart").getContext('2d');
+
+let aSpdChart = new Chart(airSpeedChart, {
+  type: 'line',
+  data: {
+    labels: timeSorted,
+    datasets: [{
+      label: 'AirSpeed (knots)',
+      data: aSpdData,
+      fill: false,
+      borderColor: 'red',
+      borderWidth: '0.5'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'AirSpeed (knots)'
+        }
+      }],
+      xAxes: [{
+        display: true,
+        type: 'time',
+      }]
+    },
+    legend: {
+      postition: 'bottom',
+      display: false
+    }
+  }
+});
+
+var vSpdData = [];
+for(var j = 0; j < jsonData.mock_data.length; j++) {
+  vSpdData.push({t: timeSorted[j], y: jsonData.mock_data[j].VerticalSpeed_ms});
+}
+
+let vertSpeedChart = document.getElementById("vertSpeedChart").getContext('2d');
+
+let vSpdChart = new Chart(vertSpeedChart, {
+  type: 'line',
+  data: {
+    labels: timeSorted,
+    datasets: [{
+      label: 'Vertical Speed (ms)',
+      data: vSpdData,
+      fill: false,
+      borderColor: 'red',
+      borderWidth: '0.5'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Vertical Speed (ms)'
+        }
+      }],
+      xAxes: [{
+        display: true,
+        type: 'time',
+      }]
+    },
+    legend: {
+      postition: 'bottom',
+      display: false
+    }
+  }
+});
+
 }
